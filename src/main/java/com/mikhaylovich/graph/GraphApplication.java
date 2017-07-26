4d0a67a93ccfa1962ccfe97b1a36109e4df72322
Mikhaylovich Dmitry <dmitry.mikhailovich@gmail.com>
2017-07-26 12:11:57 +0300

    start project

---
+++ src/main/java/com/mikhaylovich/graph/GraphApplication.java
@@ -0,0 +1,13 @@
+package com.mikhaylovich.graph;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author dmitry.mikhaylovich@bostongene.com
+ */
+@Slf4j
+public class GraphApplication {
+    public static void main(String[] args) {
+        log.info("it works");
+    }
+}
---
+++ src/main/java/com/mikhaylovich/graph/domain/model/Vertex.java
@@ -0,0 +1,7 @@
+package com.mikhaylovich.graph.domain.model;
+
+/**
+ * @author dmitry.mikhaylovich@bostongene.com
+ */
+public class Vertex {
+}
